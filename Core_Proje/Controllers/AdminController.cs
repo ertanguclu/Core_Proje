334d046444b81a3c523e333765cba36b75c55024
Ertan GÜÇLÜ <ertan.guclu@std.yildiz.edu.tr>
2023-12-20 16:46:25 +0300

    Navigation Kısmına Sayfa Adı Yazdırma

--- Core_Proje/Controllers/AdminController.cs
+++ Core_Proje/Controllers/AdminController.cs
@@ -24,5 +24,9 @@ namespace Core_Proje.Controllers
         { 
             return PartialView();
         }
+        public PartialViewResult NavigationPartial() 
+        { 
+            return PartialView();
+        }
     }
 }
