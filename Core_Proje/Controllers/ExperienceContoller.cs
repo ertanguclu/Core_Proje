b29d63b6d71005f7f8ce1318f612b451a277a9cb
Ertan GÜÇLÜ <ertan.guclu@std.yildiz.edu.tr>
2023-12-20 18:18:40 +0300

    Deneyimlerin listelenmesi

---
+++ Core_Proje/Controllers/ExperienceContoller.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Controllers
+{
+    public class ExperienceContoller : Controller
+    {
+        ExperienceManager experienceManager = new ExperienceManager(new EfExperienceDal());
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Deneyim Listesi";
+            ViewBag.v2 = "Deneyimler";
+            ViewBag.v3 = "Deneyim Listesi";
+            var values = experienceManager.TGetList();
+            return View(values);
+        }
+    }
+}
