2d2ebdbb44100542488eeb99163644da2f82e9df
Ertan GÜÇLÜ <ertan.guclu@std.yildiz.edu.tr>
2023-12-22 19:20:14 +0300

    Portfolio ekleme işlemleri

--- Core_Proje/Controllers/PortfolioController.cs
+++ Core_Proje/Controllers/PortfolioController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.Controllers
@@ -14,6 +15,20 @@ namespace Core_Proje.Controllers
             ViewBag.v3 = "Proje Listesi";
             var values=portfolioManager.TGetList();
             return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddPortfolio()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddPortfolio(Portfolio p)
+        {
+            ViewBag.v1 = "Proje Listesi";
+            ViewBag.v2 = "Projelerim";
+            ViewBag.v3 = "Proje Ekleme";
+            portfolioManager.TAdd(p);
+            return RedirectToAction("Index");
         }
     }
 }
