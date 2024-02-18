18fcd4016ae00d508d570f174b841c8dc476e2f6
Ertan GÜÇLÜ <ertan.guclu@std.yildiz.edu.tr>
2024-02-18 13:21:53 +0300

    Son 5 Proje Listesi

---
+++ Core_Proje/ViewComponents/Dashboard/Last5Projects.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.ViewComponents.Dashboard
+{
+    public class Last5Projects : ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+    
+}
