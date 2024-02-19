70a80b27a53520277e6797523b421acddec0b054
Ertan GÜÇLÜ <ertan.guclu@std.yildiz.edu.tr>
2024-02-19 17:35:12 +0300

    Register Backend

--- Core_Proje/Areas/Writer/Models/UserRegisterViewModel.cs
+++ Core_Proje/Areas/Writer/Models/UserRegisterViewModel.cs
@@ -5,6 +5,12 @@ namespace Core_Proje.Areas.Writer.Models
 {
     public class UserRegisterViewModel
     {
+        [Required(ErrorMessage = "Lütfen Adınızı Girin")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Lütfen Soyadınızı Girin")]
+        public string Surname { get; set; }
+        [Required(ErrorMessage = "Lütfen resim url değeri Girin")]
+        public string ImageUrl { get; set; }
         [Required(ErrorMessage ="Lütfen Kullanıcı Adını Girin")]
         public string UserName { get; set; }
         [Required(ErrorMessage = "Lütfen şifreyi girin")]
