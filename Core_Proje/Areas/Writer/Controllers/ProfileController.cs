66d0554a47d825496f7ca1a752f06b65fafccd8c
Ertan GÜÇLÜ <ertan.guclu@std.yildiz.edu.tr>
2024-02-20 16:09:40 +0300

    Update user View Model

--- Core_Proje/Areas/Writer/Controllers/ProfileController.cs
+++ Core_Proje/Areas/Writer/Controllers/ProfileController.cs
@@ -1,6 +1,9 @@
-﻿using EntityLayer.Concrete;
+﻿using Core_Proje.Areas.Writer.Models;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Core_Proje.Areas.Writer.Controllers
@@ -15,10 +18,39 @@ namespace Core_Proje.Areas.Writer.Controllers
             _userManager = userManager;
         }
 
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             var values=await _userManager.FindByNameAsync(User.Identity.Name);
-            return View(values);
+            UserEditViewModel model = new UserEditViewModel();
+            model.Name=values.Name;
+            model.Surname=values.Surname;
+            model.PictureURL = values.ImageUrl;
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditViewModel p)
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            if(p.Picture != null)
+            {
+                var resource=Directory.GetCurrentDirectory();
+                var extension = Path.GetExtension(p.Picture.FileName);
+                var imagename = Guid.NewGuid() + extension;
+                var savelocation = resource + "/wwwroot/userimage/" + imagename;
+                var stream = new FileStream(savelocation, FileMode.Create);
+                await p.Picture.CopyToAsync(stream);
+                user.ImageUrl = imagename;
+            }
+            user.Name = p.Name;
+            user.Surname = p.Surname;
+            var result = await _userManager.UpdateAsync(user);
+            if(result.Succeeded)
+            {
+                return RedirectToAction("Index", "Default");
+            }
+            return View();
         }
     }
 }
