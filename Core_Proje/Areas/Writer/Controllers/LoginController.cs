303eb20dbcb9f14c3109857d48a430b49c40c7e7
Ertan GÜÇLÜ <ertan.guclu@std.yildiz.edu.tr>
2024-02-20 12:51:11 +0300

    Login Teması

---
+++ Core_Proje/Areas/Writer/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<WriterUser> signInManager;
+
+        public LoginController(SignInManager<WriterUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Core_Proje/Areas/Writer/Models/UserLoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Core_Proje.Areas.Writer.Models
+{
+    public class UserLoginViewModel
+    {
+        [Display(Name ="Kullanıcı Adı")]
+        [Required(ErrorMessage ="Kullanıcı Adını Giriniz...!")]
+        public string Username { get; set; }
+
+        [Display(Name = "Şifre")]
+        [Required(ErrorMessage = "Şifreyi Giriniz...!")]
+        public string Password { get; set; }
+    }
+}
