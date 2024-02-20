113e27fe5ca9902baad867f64a9165d667bb1fb1
Ertan GÜÇLÜ <ertan.guclu@std.yildiz.edu.tr>
2024-02-20 13:28:41 +0300

    Login İşlemi

--- Core_Proje/Areas/Writer/Controllers/LoginController.cs
+++ Core_Proje/Areas/Writer/Controllers/LoginController.cs
@@ -1,21 +1,40 @@
-﻿using EntityLayer.Concrete;
+﻿using Core_Proje.Areas.Writer.Models;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Core_Proje.Areas.Writer.Controllers
 {
     [Area("Writer")]
     public class LoginController : Controller
     {
-        private readonly SignInManager<WriterUser> signInManager;
+        private readonly SignInManager<WriterUser> _signInManager;
 
         public LoginController(SignInManager<WriterUser> signInManager)
         {
-            this.signInManager = signInManager;
+            _signInManager = signInManager;
         }
-
+        [HttpGet]
         public IActionResult Index()
         {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(UserLoginViewModel p)
+        {
+            if(ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(p.Username,p.Password, true, true);
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Default");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Hatalı kullanıcı adı veya şifre");
+                }
+            }
             return View();
         }
     }
