173ba1337765e2f0ca08d8a56620101365a5042b
Ertan GÜÇLÜ <ertan.guclu@std.yildiz.edu.tr>
2024-02-21 15:48:44 +0300

    Layout Bildirim Ayarı

---
+++ Core_Proje/Areas/Writer/ViewComponents/Notification.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Areas.Writer.ViewComponents
+{
+    public class Notification:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
