b87e8c5d0eac6d3a855276b86d62ac3e7478c928
Ertan GÜÇLÜ <ertan.guclu@std.yildiz.edu.tr>
2024-02-22 15:31:57 +0300

    Şifre Güncelleme

--- Core_Proje/Areas/Writer/Controllers/ProfileController.cs
+++ Core_Proje/Areas/Writer/Controllers/ProfileController.cs
@@ -45,6 +45,7 @@ namespace Core_Proje.Areas.Writer.Controllers
             }
             user.Name = p.Name;
             user.Surname = p.Surname;
+            user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, p.Password);
             var result = await _userManager.UpdateAsync(user);
             if(result.Succeeded)
             {
