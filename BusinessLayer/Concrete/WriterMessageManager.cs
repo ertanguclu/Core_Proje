394d940eed6d775c42072d1bbb8c4da8e714f902
Ertan GÜÇLÜ <ertan.guclu@std.yildiz.edu.tr>
2024-02-22 11:19:09 +0300

    Mesaj Katman İşlemleri

---
+++ BusinessLayer/Concrete/WriterMessageManager.cs
@@ -0,0 +1,51 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class WriterMessageManager:IWriterMessageService
+    {
+        IWriterMessageDal _writerMessageDal;
+
+        public WriterMessageManager(IWriterMessageDal writerMessageDal)
+        {
+            _writerMessageDal = writerMessageDal;
+        }
+
+        public void TAdd(WriterMessage t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(WriterMessage t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public WriterMessage TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<WriterMessage> TGetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<WriterMessage> TGetListbyFilter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(WriterMessage t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
