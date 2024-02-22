a325aee285cda122bbad8a48a15e1cfd96370c4e
Ertan GÜÇLÜ <ertan.guclu@std.yildiz.edu.tr>
2024-02-22 15:03:43 +0300

    Mesaj Gönderirken Alıcı Adının Eklenmesi

--- Core_Proje/Areas/Writer/Controllers/MessageController.cs
+++ Core_Proje/Areas/Writer/Controllers/MessageController.cs
@@ -1,9 +1,11 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Core_Proje.Areas.Writer.Controllers
@@ -58,6 +60,9 @@ namespace Core_Proje.Areas.Writer.Controllers
             p.Date=Convert.ToDateTime(DateTime.Now.ToShortDateString());
             p.Sender = mail;
             p.SenderName = name;
+            Context c = new Context();
+            var usernamesurname = c.Users.Where(x => x.Email == p.Receiver).Select(y => y.Name + " " + y.Surname).FirstOrDefault();
+            p.ReceiverName = usernamesurname;
             writerMessageManager.TAdd(p);
             return RedirectToAction("SenderMessage", "Message");
 
