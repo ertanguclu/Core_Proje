5485f1cc66a19fbdeb891152563bfe2aef04863b
Ertan GÜÇLÜ <ertan.guclu@std.yildiz.edu.tr>
2024-02-22 13:43:56 +0300

    Giden Mesaj Detayları

--- Core_Proje/Areas/Writer/Controllers/MessageController.cs
+++ Core_Proje/Areas/Writer/Controllers/MessageController.cs
@@ -32,8 +32,13 @@ namespace Core_Proje.Areas.Writer.Controllers
             var messageList = writerMessageManager.GetListSenderMessage(p);
             return View(messageList);
         }
-        [HttpGet]
         public IActionResult MessageDetails(int id)
+        {
+            WriterMessage writerMessage = writerMessageManager.TGetByID(id);
+            return View(writerMessage);
+        }
+
+        public IActionResult ReceiverMessageDetails(int id)
         {
             WriterMessage writerMessage = writerMessageManager.TGetByID(id);
             return View(writerMessage);
