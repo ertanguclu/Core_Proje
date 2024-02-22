0a1375c0563c39d46f35d930ccd049a603226002
Ertan GÜÇLÜ <ertan.guclu@std.yildiz.edu.tr>
2024-02-22 21:43:42 +0300

    Admin Sender Message

---
+++ Core_Proje/Controllers/AdminMessageController.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Controllers
+{
+    public class AdminMessageController : Controller
+    {
+        WriterMessageManager WriterMessageManager = new WriterMessageManager(new EfWriterMessageDal());
+        public IActionResult ReceiverMessageList()
+        {
+            string p;
+            p = "admin@gmail.com";
+            var values = WriterMessageManager.GetListReceiverMessage(p);
+            return View(values);
+        }
+        public IActionResult SenderMessageList()
+        {
+            string p;
+            p = "admin@gmail.com";
+            var values = WriterMessageManager.GetListSenderMessage(p);
+            return View(values);
+        }
+        public IActionResult AdminMessageDetails(int id)
+        {
+            var values = WriterMessageManager.TGetByID(id);
+            return View(values);
+        }
+    }
+}
