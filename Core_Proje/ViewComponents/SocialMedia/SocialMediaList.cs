5d471868bdffed5fb5f83a8fc7b0deef914206b6
Ertan GÜÇLÜ <ertan.guclu@std.yildiz.edu.tr>
2024-02-23 19:20:07 +0300

    Default Sosyal Medya Component View

---
+++ Core_Proje/ViewComponents/SocialMedia/SocialMediaList.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.ViewComponents.SocialMedia
+{
+    public class SocialMediaList:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
