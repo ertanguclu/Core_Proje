1819eb893cf3f272f2b6f4f1ab2dddf686176313
Ertan GÜÇLÜ <ertan.guclu@std.yildiz.edu.tr>
2024-02-25 16:38:33 +0300

    Dashboard

--- Core_Proje/Controllers/DashboardController.cs
+++ Core_Proje/Controllers/DashboardController.cs
@@ -6,9 +6,6 @@ namespace Core_Proje.Controllers
     {
         public IActionResult Index()
         {
-            ViewBag.v1 = "Dashboard";
-            ViewBag.v2 = "İstatistikler";
-            ViewBag.v3 = "İstatistik Sayfası";
             return View();
         }
     }
