7547f47b51a458d22e94b38695f72f6663985a50
Ertan GÜÇLÜ <ertan.guclu@std.yildiz.edu.tr>
2024-02-25 17:31:24 +0300

    Proje Seviyesinde Authorize

--- Core_Proje/Controllers/AdminLoginController.cs
+++ Core_Proje/Controllers/AdminLoginController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.Controllers
 {
+    [AllowAnonymous]
     public class AdminLoginController : Controller
     {
         public IActionResult Index()
