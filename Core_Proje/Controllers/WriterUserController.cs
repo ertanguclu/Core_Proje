6146836ba952f1933b0500d50dd9e8c258e55ea3
Ertan GÜÇLÜ <ertan.guclu@std.yildiz.edu.tr>
2024-02-26 14:55:11 +0300

    Revize Hazırlığı

--- Core_Proje/Controllers/WriterUserController.cs
+++ Core_Proje/Controllers/WriterUserController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Collections.Generic;
@@ -17,6 +18,13 @@ namespace Core_Proje.Controllers
         public IActionResult ListUser()
         {
             var values = JsonConvert.SerializeObject(userManager.TGetList());
+            return Json(values);
+        }
+        [HttpPost]
+        public IActionResult AddUser(WriterUser p)
+        {
+            userManager.TAdd(p);
+            var values = JsonConvert.SerializeObject(p);
             return Json(values);
         }
     }
