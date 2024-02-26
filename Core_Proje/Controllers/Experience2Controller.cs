549c19a42fe0b37f06ac394da54ec7c0cd7bfaa0
Ertan GÜÇLÜ <ertan.guclu@std.yildiz.edu.tr>
2024-02-26 18:44:07 +0300

    Ajax Script Required Fals

--- Core_Proje/Controllers/Experience2Controller.cs
+++ Core_Proje/Controllers/Experience2Controller.cs
@@ -1,9 +1,12 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Core_Proje.Controllers
 {
@@ -12,7 +15,6 @@ namespace Core_Proje.Controllers
         ExperienceManager experienceManager = new ExperienceManager(new EfExperienceDal());
         public IActionResult Index()
         {
-
             return View();
         }
         public IActionResult ListExperience()
@@ -27,10 +29,23 @@ namespace Core_Proje.Controllers
             var values = JsonConvert.SerializeObject(p);
             return Json(values);
         }
-        public IActionResult GEtById(int ExperienceID)
+        public IActionResult GetById(int ExperienceID)
         {
             var v = experienceManager.TGetByID(ExperienceID);
-            var values=JsonConvert.SerializeObject(v);
+            var values = JsonConvert.SerializeObject(v);
+            return Json(values);
+        }
+        public IActionResult DeleteExperience(int id)
+        {
+            var v = experienceManager.TGetByID(id);
+            experienceManager.TDelete(v);
+            return NoContent();
+        }
+        public IActionResult UpdateExperince(Experience p)
+        {
+            var v = experienceManager.TGetByID(p.ExperienceID);
+            experienceManager.TUpdate(v);
+            var values = JsonConvert.SerializeObject(p);
             return Json(values);
         }
     }
