f2054e6606279d25f8980656a1ed7af21afc7358
Ertan GÜÇLÜ <ertan.guclu@std.yildiz.edu.tr>
2024-02-26 14:26:07 +0300

    Ajax

--- Core_Proje/Controllers/WriterUserController.cs
+++ Core_Proje/Controllers/WriterUserController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Core_Proje.Controllers
 {
     public class WriterUserController : Controller
     {
+        WriterUserManager userManager = new WriterUserManager(new EfWriterUserDal());
         public IActionResult Index()
         {
+            
             return View();
+        }
+        public IActionResult ListUser()
+        {
+            var values = JsonConvert.SerializeObject(userManager.TGetList());
+            return Json(values);
         }
     }
 }
