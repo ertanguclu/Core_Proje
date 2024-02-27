5044f0f9cc69a90c3b62834406c5fa02ef854650
Ertan GÜÇLÜ <ertan.guclu@std.yildiz.edu.tr>
2024-02-27 11:45:02 +0300

    Apilerle Get İsteği

---
+++ Core_Proje_Api/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using Core_Proje_Api.DAL.ApiContext;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Core_Proje_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            var c = new Context();
+            return Ok(c.Categories.ToList());
+        }
+    }
+}
